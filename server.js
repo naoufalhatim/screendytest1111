4aeb9322b227117aabbd1521b59fd1ca3be05ddd
naoufalhatim <naoufal.hatim@usmba.ac.ma>
2016-04-29 06:42:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,10 @@ app.post('/addUser', function(req, res){
 
 
 //----------------------------------------------------------------
-
+db.collection("User").authenticate('user', 'name', function(err, result) {
+      assert.equal(true, result);
+}
+      
 //___________________________________________________________________
 
 
